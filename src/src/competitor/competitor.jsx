897e97269851148548578b8cef3e7c7024e2c292
tyjta <tyjtanner@gmail.com>
2025-02-12 19:55:47 -0700

    jsx files added for about, admin, events, competitor and login with an export function added to each.

---
+++ src/src/competitor/competitor.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Competitor() {
+    return (
+        <main className="container-fluid bg-secondary text-center">
+            <div>Competitor displayed here</div>
+        </main>
+    );
+}
---
+++ src/src/events/events.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Events() {
+    return (
+        <main className="container-fluid bg-secondary text-center">
+            <div>Events displayed here</div>
+        </main>
+    );
+}
